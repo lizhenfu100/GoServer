ec272dba157ec456de44aee9e86dd75f280f40c4
3workman <734688714@qq.com>
2018-01-20 19:37:31 +0800

    Debug：多线程id重复

--- src/dbmgo/db_inc_id.go
+++ src/dbmgo/db_inc_id.go
@@ -7,22 +7,24 @@ import (
 )
 
 var (
-	g_inc_id_map sync.Map
+	g_inc_id_map   = make(map[string]uint32)
+	g_inc_id_mutex sync.Mutex
 )
 
 func _init_inc_ids() {
 	var lst []common.KeyPair
 	FindAll("IncId", nil, &lst)
+	g_inc_id_mutex.Lock()
 	for _, v := range lst {
-		g_inc_id_map.Store(v.Name, uint32(v.ID))
+		g_inc_id_map[v.Name] = uint32(v.ID)
 	}
+	g_inc_id_mutex.Unlock()
 }
-func GetNextIncId(key string) (ret uint32) {
-	ret = 1
-	if v, ok := g_inc_id_map.Load(key); ok {
-		ret += v.(uint32)
-	}
-	g_inc_id_map.Store(key, ret)
+func GetNextIncId(key string) uint32 {
+	g_inc_id_mutex.Lock()
+	ret := g_inc_id_map[key] + 1 //Debug：实际包含三步：读出、+1、写入，必原子的完成，才可保证每次返回不同id；sync.Map仅保障了读写安全性
+	g_inc_id_map[key] = ret
+	g_inc_id_mutex.Unlock()
 	if ret == 1 {
 		InsertToDB("IncId", common.KeyPair{key, 1})
 	} else {
